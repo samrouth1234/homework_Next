c2cec77d0e57b06ca266aa5a72afcb5f0737a1c8
samrouth <bichsamroth@gmail.com>
2023-05-05 14:59:50 +0700

    Next.js

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -4,12 +4,30 @@ import DataTable from 'react-data-table-component';
 
 export default function MyComponent() {
     const [data, setData] = useState([]);
+    const [searchApiData,setSearchApiData]=useState([]);
+    const[filterVal,setFilterVal]=useState("");
     useEffect(() => {
-        fetch('https://api.escuelajs.co/api/v1/products')
+        const fetchData=()=>{
+            fetch(`https://api.escuelajs.co/api/v1/products`)
             .then((response) => response.json()
-                .then((data) => setData(data))
+                .then(json => {
+                    setData(json)
+                    setSearchApiData(json)
+                })
             )
+        }
+        fetchData();
     }, []);
+    const handleFilter=(e)=>{
+        if(e.target.value == ''){
+            setData(searchApiData)
+        }else{
+           const filterResult= searchApiData.filter (item => item.title.toLowerCase(e.target.value.toLowerCase())|| item.price.toLowerCase(e.target.value.toLowerCase())||item.description.toLowerCase(e.target.value.toLowerCase()))
+           setData(filterResult)
+        }
+        setFilterVal(e.target.value)
+    }
+
     const columns = [
         {
             name: 'Product',
@@ -21,24 +39,23 @@ export default function MyComponent() {
         },
         {
             name: 'Category',
-            selector: row => row.description,
+            selector: row => row.description.slice(0, 30),
         },
         {
             name: 'Photo',
-		    cell: row =><img height="70px" width="60px" alt={row.name} src={row.images} />,
+            cell: row => <img height="70px" width="60px" alt={row.name} src={row.images} />,
         },
         {
             name: 'Action',
-            cell: row => <div> <button type="button" class="btn btn-primary">Edit</button> <button type="button" class="btn btn-danger">Delete</button></div>,
+            cell: row => <><button type="button" class="btn btn-primary m-2">Edit</button><button type="button" class="btn btn-danger">Delete</button></>,
         },
     ];
-
     return (
         <div className='container mt-5'>
             <div>
                 <h1>Product Collectoin List</h1>
-                <div className='text-end'>
-                    <input type="text" name="search" placeholder="Search.." className='p-2 rounded'/>
+                <div className="text-end">
+                <input className="p-2 w-25 " type="text" placeholder="Search product list" value={filterVal} onInput={(e)=>handleFilter(e)}/>
                 </div>
             </div>
             <DataTable
@@ -46,9 +63,7 @@ export default function MyComponent() {
                 columns={columns}
                 data={data}
                 pagination
-                fixedHeader
-                fixedHeaderScrollHeight="500px"
             />
         </div>
     );
-};
+}
